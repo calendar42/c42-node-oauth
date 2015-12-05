ea9074b9b89c6a48632a2c51708a3382882ad05d
Edmon <comlaterra@gmail.com>
2015-12-05 12:27:53 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # c42-node-oauth
 Node JS oAuth consumer for the C42 REST API
+
+This is an example project about how to consume the Calendar42 oAuth.
+
+It is using nodeJs technologies and the API [node-oauth](https://github.com/ciaranj/node-oauth)
+
+# Installation
+
+Clone this project
+
+```
+git clone https://github.com/calendar42/c42-node-oauth.git
+```
+
+Install node-oauth
+
+```
+npm install
+```
+
+Add your clientID, clientSecret and serviceID
+
+```javascript
+...
+var clientID = '';
+var clientSecret = '';
+var serviceGUID = '';
+...
+```
+
+# Usage
+
+Run your project
+
+```
+node oAuth.js
+```
+
+Visit the page with your browser. The server is listening in the port 8080.
+
+```
+localhost:8080
+```
+
+Clicking the link you will start the oAuth process.
+The last page should show the Calendars list of the user used to login.
---
+++ oAuth.js
@@ -0,0 +1,114 @@
+var http = require('http');
+var qs = require('querystring');
+var OAuth = require('oauth'), OAuth2 = OAuth.OAuth2;
+
+var clientID = '';
+var clientSecret = '';
+var serviceGUID = '';
+
+var host = 'https://demo.calendar42.com/';
+var authorizePath = 'oauth2/authorize/';
+var tokenPath = 'oauth2/token/';
+
+var calendarsPath = 'api/v2/events/';
+
+var oauth2 = new OAuth2(clientID,
+                        clientSecret,
+                        host,
+                        authorizePath,
+                        tokenPath,
+                        null); /** Custom headers */
+
+http.createServer(function (req, res) {
+    var p = req.url.split('/');
+    pLen = p.length;
+    /**
+     * Authorised url as per github docs:
+     * https://developer.github.com/v3/oauth/#redirect-users-to-request-github-access
+     *
+     * getAuthorizedUrl: https://github.com/ciaranj/node-oauth/blob/master/lib/oauth2.js#L148
+     * Adding params to authorize url with fields as mentioned in github docs
+     *
+     */
+    var authURL = oauth2.getAuthorizeUrl({
+        // redirect_uri: 'http://localhost:8080/code',
+        scope: ['service.read'],
+        state: 'random_state_string',
+        response_type: 'code'
+    });
+
+    var access_token;
+
+    /**
+     * Creating an anchor with authURL as href and sending as response
+     */
+    var body = '<a href="' + authURL + '"> Get C42 Code </a>';
+    if (pLen === 2 && p[1] === '') {
+        console.log("link page requested")
+        res.writeHead(200, {
+            'Content-Length': body.length,
+            'Content-Type': 'text/html' });
+        res.end(body);
+    } else if (pLen === 3 && p[1].indexOf('code') === 0) {
+        // Calendar42 sends the oAuth Code in the url
+        var url = require('url');
+        var url_parts = url.parse(req.url, true);
+        var query = url_parts.query;
+
+        var code = query.code;
+
+        /** Obtaining access_token */
+        oauth2.getOAuthAccessToken(
+            code,
+            {
+              'grant_type':'authorization_code',
+              'redirect_uri': 'http://localhost:8080/code/',
+              'service': serviceGUID
+            },
+            function (e, resp_access_token, refresh_token, results){
+                if (e) {
+                    // Error 1
+                    console.log(e);
+                    res.end(e);
+                } else if (results.error) {
+                    // Error 2
+                    console.log(results);
+                    res.end(JSON.stringify(results));
+                }else {
+                    // Success
+                    access_token = resp_access_token;
+                    console.log('Obtained access_token: ', access_token);
+                    getCalendars();
+                }
+        });
+
+        // example method about how to use the accessToken once it is get
+        var getCalendars = function(){
+
+          oauth2.useAuthorizationHeaderforGET(true);
+
+          oauth2.get(host + calendarsPath, access_token, function(response){
+            var body = "";
+            var data = JSON.parse(response.data);
+            if(response.statusCode === 200){
+              // error
+              body = "<p><b>StatusCode</b> = "+response.statusCode+"</p>" +
+                "<p><b>Message</b> = "+data.error.message+"</p>";
+            }else{
+              var renderData = JSON.stringify(data.data);
+              body = '<p><b>StatusCode</b> = '+response.statusCode+'</p><p>Found = '+data.meta_data.count+' calendars</p><p><b>Calendars list</b> : '+renderData+'</p>';
+            }
+            res.writeHead(200, {
+                'Content-Length': body.length,
+                'Content-Type': 'text/html' });
+            res.end(body);
+          });
+        }
+    } else {
+      body = "Not Found";
+      res.writeHead(404, {
+          'Content-Length': body.length,
+          'Content-Type': 'text/html' });
+      res.end(body);
+    }
+}).listen(8080);
